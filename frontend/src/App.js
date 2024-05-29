7be6bd96e5cf92dc7dec24ce8ce73945d0a7349c
Salman <salman26080@gmail.com>
2024-05-29 21:17:11 +0800

    Improving the homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,19 @@ const App = () => {
   return (
     <div className="App">
       <img src="/logo.png" alt="Logo" className="logo" />
-      <div className="buttons-container">
+      <div className="nav-container">
         <button className="button button-link">Leaderboard</button>
         <button className="button button-link">Activities</button>
         <button className="button button-link">Learn More</button>
       </div>
-      <p className='text'>Connecting Communities One Activity at a Time - Create, Join, and Enjoy.</p>
+      <div className="hero-content">
+        <p className='hero-text'>Connecting Communities One Activity at a Time - Create, Join, and Enjoy.</p>
+        <button className='button get-started-button'>Get Started</button>
+      </div>
     </div>
   );
 };
 
 export default App;
+
+
