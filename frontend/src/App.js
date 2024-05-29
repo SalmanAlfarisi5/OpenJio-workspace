f9b052e0baa0bd4513e113d9d5dd7e461ae71dd2
Salman <salman26080@gmail.com>
2024-05-29 18:30:59 +0800

    Creating the homapage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <img src="/logo.png" alt="Logo" className="logo" />
+      <div className="buttons-container">
+        <button className="button button-link">Leaderboard</button>
+        <button className="button button-link">Activities</button>
+        <button className="button button-link">Learn More</button>
+      </div>
+      <p className='text'>Connecting Communities One Activity at a Time - Create, Join, and Enjoy.</p>
     </div>
   );
 };
