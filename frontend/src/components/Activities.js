7d684b11cfd1059800e9989b14c47c486d7c5583
Salman <salman26080@gmail.com>
2024-05-30 16:22:04 +0800

    improving the activity page

--- frontend/src/components/Activities.js
+++ frontend/src/components/Activities.js
@@ -23,9 +23,26 @@ const Activities = () => {
         <button className="button button-link">Learn More</button>
       </div>
       <div className="blocks">
-        <div className="activity-block"></div>
-        <div className="activity-block"></div>
-        <div className="activity-block"></div>
+        <div className="activity-block">
+          <img alt="soccer" className="activity-image" src="/soccer.jpg"></img>
+          <h2 className="activity-title">Soccer Match</h2>
+        </div>
+        <div className="activity-block">
+          <img
+            alt="watching a movie"
+            className="activity-image"
+            src="/movies.jpg"
+          ></img>
+          <h2 className="activity-title">Watching a Movie</h2>
+        </div>
+        <div className="activity-block">
+          <img
+            alt="House Party"
+            className="activity-image"
+            src="./house-party.jpg"
+          ></img>
+          <h2 className="activity-title">House Party</h2>
+        </div>
       </div>
     </div>
   );
