5069cf93afa367a2f39aa78474aa28f57449f583
Judha Hoka Wishika <judha38@gmail.com>
2024-06-01 13:36:02 +0800

    Updated frontend
    versi terbaru yang bisa masukkin input ke database

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,23 +1,61 @@
-// src/components/Login.js
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Login.css";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8080/api/login', formData);
+      console.log('Login successful:', response.data);
+      setSuccess('Login successful!');
+      setError('');
+      // Optionally handle successful login (e.g., redirect to dashboard)
+    } catch (error) {
+      if (error.response && error.response.data) {
+        setError(error.response.data.error);
+        setSuccess('');
+      } else {
+        console.error('Error logging in:', error);
+        setError('An unexpected error occurred.');
+        setSuccess('');
+      }
+    }
+  };
+
   return (
     <div className="login-container">
       <h2>Login Page</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
+        {error && <div className="error-message">{error}</div>}
+        {success && <div className="success-message">{success}</div>}
         <div>
           <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" required />
+          <input type="text" id="username" name="username" value={formData.username} onChange={handleChange} required />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
+          <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required />
         </div>
         <button type="submit">Login</button>
       </form>
-      <br></br>
+      <br />
       <div>
         <p>
           Don't have an account? <a href="/register">Register</a>
