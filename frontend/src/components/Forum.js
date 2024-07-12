84474d42d136b555e5570af00f6b72c5e03d5201
Salman <salman26080@gmail.com>
2024-07-12 15:39:28 +0800

    Creating forum page

---
+++ frontend/src/components/Forum.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Forum = () => {
+  return (
+    <div>
+      <h1>Forum</h1>
+      <p>Welcome to the Forum page!</p>
+    </div>
+  );
+};
+
+export default Forum;
